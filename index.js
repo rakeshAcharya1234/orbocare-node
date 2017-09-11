421eeadbd20e27df5ef3dc2d8c04dbccac5d3cc9
Rakesh <rakesh@orbonix.com>
2017-09-11 16:37:10 +0530

    Get Ip address

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require('http');
 var url = require("url");
-
-const hostname = '127.0.0.1';
+var ip = require("ip");
+const hostname = ip.address();
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   var parsedUrl = url.parse(req.url, true); // true to get query as object
   var queryAsObject = parsedUrl.query;
+
   res.end(JSON.stringify(queryAsObject));
 });
 server.listen(port, hostname, () => {
